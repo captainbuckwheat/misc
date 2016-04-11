4d3ce3a27b85accbf6df520ca916f91ea772e20c
AK <captainbuckwheat@gmail.com>
2016-04-11 16:26:20 -0700

    Create cube_probability.js

---
+++ cube_probability.js
@@ -0,0 +1,6 @@
+// a) we have a cube with 4 blue faces and 2 red faces. what's the probability that when it's rolled,a blue face will be facing up?
+// (AOPS Counting & Probability)
+
+var cube_probability = function(outcome_sides) { // same as a normal 6 sided dice_probability function
+	return outcome_sides/6; 
+}
