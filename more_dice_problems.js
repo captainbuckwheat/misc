0dbf22adaeb0a4f85e8dc40f9818933dafd46d78
AK <captainbuckwheat@gmail.com>
2016-04-13 15:02:59 -0700

    Update more_dice_problems.js

--- more_dice_problems.js
+++ more_dice_problems.js
@@ -1,8 +1,10 @@
-/* Here are some cool questions! (AOPS Counting & Probability)
+/* Here are some cool dice questions! 
 
-a) suppose we have 2 fair six-sided dice. if we roll them both what is the probabilty the two nubers shown sum up to 7?
+a) suppose we have 2 fair six-sided dice. if we roll them both what is the probabilty the two nubers shown sum up to 7? (AOPS Counting & Probability)
+b) on average, how many times must a 6-sided die be rolled until a 6 turns up?
 */
 
+//a)
 var sum_dice = function(dice, sum, n) { //where sum is the sum of the dice; dice is # of dice & n is # of cases we roll the dice simultaneously
 	var dice_outcome, count, i, j, add; 
 	count = 0;
@@ -33,3 +35,41 @@ var sum_dice_1 = function(dice, n) { //where dice is # of dice & n is # of cases
 	for (i = 0; i < count.length; i++) count[i] /= n; //returns an array of all probabilities for sums 0 through through 6 * # of dice
 	return count;
 }
+
+//b)
+
+var dice_side = function (a,sides) {
+	var i, j, k, success_rolls_k, success_rolls, side, count, roll, sum, k_sum; 
+	success_rolls_k = [];
+	sum_k = 0;
+	for (k = 0; k < 10; k++){ //run the program k times and you will see that once in a while it returns different answer
+		success_rolls = []; 
+		roll = 0; 
+		j = 0;
+		sum = 0;
+		for (i = 0; i < sides*10; i++) { //for 6 sided dice it will have 60 trials per k; trialls = success rolls. 
+			while(true){
+				side = Math.floor(Math.random()*sides + 1); //assuming all sides are distinct and consecutive from 1 to sides
+				roll++; //counting every time we roll
+				if (side === a) { //in a fair dice a is irrelevant, but we leave it as a variable for a sake of an actual problem
+					success_rolls[j] = roll; // the roll on which side a showed up
+					roll = 0; 
+					j++;
+					break;
+				}
+			} 
+		}
+		for (i = 0; i < success_rolls.length; i++) {
+			sum+=success_rolls[i]; 
+		}
+		console.log("success_rolls is "+success_rolls); 
+		success_rolls_k[k] = Math.round(sum/success_rolls.length); 
+		console.log("success_rolls_k["+k+"] is "+success_rolls_k[k]); 
+	}
+	for (k = 0; k < 10; k++) {
+		sum_k+=success_rolls_k[k]; 
+	}
+	return Math.round(sum_k/10); 
+}
+//the simple answer: for a fair dice in which there's an equally likely chance of landing on any face; 
+//the answer should always be the number of the sides for any face. 
